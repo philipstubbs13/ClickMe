ede132817ae4dbccc7e139c58cac7c61e0d0fccd
Phil S <philipstubbs13@gmail.com>
2018-04-07 20:43:14 -0500

    randomizer working

--- clicker/src/App.js
+++ clicker/src/App.js
@@ -21,6 +21,13 @@ class App extends Component {
   handleIncrement = () => {
     // We always use the setState method to update a component's state
     this.setState({ count: this.state.count + 1 });
+    //Randomize 
+    //Every time an image is clicked, the images rendered to the page shuffle themselves in a random order.
+    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    for (let i = players.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [players[i], players[j]] = [players[j], players[i]];
+    }
   };
 
 
